388935ce089f6a21e119cb6d1d1524472ee1540e
mendergit5 <vyasvashist18@gmail.com>
2024-01-09 23:52:20 +0530

    MyBusinessCard Solution Added

--- week6/assignments/week5/businesscard/src/App.jsx
+++ week6/assignments/week5/businesscard/src/App.jsx
@@ -8,25 +8,36 @@ function App() {
 
   return (
     <>
-      <div>
-        <CardWrapper>
-          <TextComponent>
-            Vashist
-          </TextComponent>
-        </CardWrapper>
+      <div class="flex-container">
+          <TextComponent classname="heading">Vashist</TextComponent>
+          <TextComponent classname="caption weight-normal grey">A Student in the 100xDevs Cohort 2.0</TextComponent>
+          <TextComponent classname="sub-heading weight-bold">Interests</TextComponent>
+          <TextComponent classname="sub-text weight-normal grey">Ionic</TextComponent>
+          <TextComponent classname="sub-text weight-normal grey">Open Source</TextComponent>
+          <TextComponent classname="sub-text weight-normal grey">App Dev</TextComponent>
+          <div class='button-container'>
+          <ButtonComponent classname="button blue">LinkedIn</ButtonComponent>
+          <ButtonComponent classname="button blue margin-left">Twitter</ButtonComponent>
+          </div>
       </div>
     </>
   )
 }
 
-function CardWrapper({children}) {
-  return <div style={{ border: "2px solid black", padding:10,margin:10}}>{children}</div>
-}
+// function CardWrapper({children}) {
+//   return <div class="">{children}</div>
+// }
 
-function TextComponent({children}){
-  return <div>
+function TextComponent({classname, children}){
+  return <div class = {classname}>
     {children}
   </div>
+}
+
+function ButtonComponent({classname, children}){
+  return <button class = {classname}>
+    {children}
+  </button>
 }
 
 
